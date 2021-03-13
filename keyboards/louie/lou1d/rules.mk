aaddaa1e7c3a21aa409ab6187959974b33c3b7e1
The Louie <github@louie.se>
2021-03-13 13:30:54 +0100

    Added lou:1d

---
+++ keyboards/louie/lou1d/rules.mk
@@ -0,0 +1,24 @@
+# MCU name
+MCU = atmega32u4
+# Do not put the microcontroller into power saving mode
+# when we get USB suspend event. We want it to keep updating
+# backlight effects.
+OPT_DEFS += -DNO_SUSPEND_POWER_DOWN
+
+
+# Build Options
+#   comment out to disable the options.
+#
+BACKLIGHT_ENABLE = no              # Enable keyboard backlight functionality
+BOOTMAGIC_ENABLE = lite            # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no              # Mouse keys
+EXTRAKEY_ENABLE = no              # Audio control and System control
+CONSOLE_ENABLE = no                # Console for debug
+COMMAND_ENABLE = no                # Commands for debug and configuration
+SLEEP_LED_ENABLE = no              # Breathing sleep LED during USB suspend
+NKRO_ENABLE = no                  # USB Nkey Rollover
+AUDIO_ENABLE = no                  # Audio output on port C6
+NO_USB_STARTUP_CHECK = no          # Disable initialization only when usb is plugged in
+#SERIAL_LINK_ENABLE = yes
+
+CIE1931_CURVE = yes
